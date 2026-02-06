ca38217becb570b7674d5bb9a94a6753698066de
Abhishek sharma <abhi3112006@gmail.com>
2026-02-06 14:13:50 +0530

    Update api.py

--- api.py
+++ api.py
@@ -9,10 +9,5 @@ app = Flask(__name__)
 def home():
     return "<h1> this is my first flask app.</h1><p> its working </p>"
 
-#defining a second page(/hello)
-@app.route('/hello')
-def say_hello():
-    return "you are now on the hello page"
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
