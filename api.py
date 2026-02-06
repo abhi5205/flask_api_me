8e2c1d67427bd4a4f9ac0aaf04f59345ad71a8c3
Abhishek sharma <abhi3112006@gmail.com>
2026-02-06 14:35:04 +0530

    Fix string quotes in api.py

--- api.py
+++ api.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 #define what happens when you visit the main page (/)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "<h1> this is my first flask app.</h1><p> its working </p>"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
